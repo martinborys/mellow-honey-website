e08d127fe703c9587f7472a0abbfa38fdaa4ef26
Marcin Borysiewicz <90431193+martinborys@users.noreply.github.com>
2022-03-10 21:25:25 -0500

    feature 2 - removed header, added styling to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import "./App.css";
 import Navbar from './components/navbar/navbar';
-import Header from './components/header/header';
 
 function App() {
   return(
   <div>
-    <Header />
     <Navbar />
   </div>
   );
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './navbar.css';
+import '../../index.css'
 
 const Navbar = () => {
   return (
-
-    <nav>
-        <ul className="navbar-links">
-          <li><a className='nav-link' href="#home">Home</a></li>
-          <li><a className='nav-link' href="#music">Music</a></li>
-          <li><a className='nav-link' href="#merch">Merch</a></li>
-          <li><a className='nav-link' href="#shows">Shows</a></li>
-          <li><a className='nav-link' href="#gallery">Library</a></li>
-        </ul>
-    </nav>
+    <body>
+      <nav>
+        <div className='nav-logo'>
+          <h4>MELLOW HONEY</h4>
+        </div>
+          <ul className="navbar-links">
+            <li><a className='nav-link' href="#home">Home</a></li>
+            <li><a className='nav-link' href="#music">Music</a></li>
+            <li><a className='nav-link' href="#merch">Merch</a></li>
+            <li><a className='nav-link' href="#shows">Shows</a></li>
+            <li><a className='nav-link' href="#gallery">Gallery</a></li>
+          </ul>
+      </nav>
+    </body>
   );
 };
 
