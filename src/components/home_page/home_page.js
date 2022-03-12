3f949ad344f0cac493e583106347628516234a14
Marcin Borysiewicz <90431193+martinborys@users.noreply.github.com>
2022-03-12 18:23:09 -0500

    feature 6 - added embedded video from daily collegian

--- src/components/home_page/home_page.js
+++ src/components/home_page/home_page.js
@@ -6,7 +6,13 @@ const HomePage = () => {
     return(
         <body>
             <div className='cafe210'>
-                <img src={cafe210} alt="cafe210"/>
+                <img src={cafe210} alt='mellow honey cafe 210' />
+            </div>
+            <div className='iframe-container'>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Y6Cfx-G1QCY" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
             </div>
         </body>
     );
